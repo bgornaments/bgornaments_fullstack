3e6a3e00026cc856ee1a9dc67ed8281eeafdd8bc
22-vanshika <139971524+22-vanshika@users.noreply.github.com>
2024-06-27 10:58:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>My todoss</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
