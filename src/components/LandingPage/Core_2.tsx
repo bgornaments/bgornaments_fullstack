013f2e4e54fe74dbf4ff6c2b63865109c8c52e43
vanshika sharma <vanshikas2022@gmail.com>
2024-07-20 16:10:06 +0530

    add core_2

--- src/components/LandingPage/Core_2.tsx
+++ src/components/LandingPage/Core_2.tsx
@@ -1,82 +1,75 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+import logo from "/src/assets/image.png";
 import img21 from "/src/assets/Group 28.png";
-import img3 from "/src/assets/img3.svg"
-import img4 from "/src/assets/img3.png"
-import img7 from "/src/assets/img7.svg"
-import img8 from "/src/assets/img8.svg"
-import img9 from "/src/assets/img9.svg"
-import Navbar from './Navbar';
+import img3 from "/src/assets/img3.svg";
+import img7 from "/src/assets/img7.svg";
+import img8 from "/src/assets/img8.svg";
+import img9 from "/src/assets/img9.svg";
+import Navbar from "./Navbar";
 
-
-const Core: React.FC = () => {
+const Core_2: React.FC = () => {
   return (
     <>
-    <Navbar/>
-    <div className="bg-[#fff9f5] w-full min-h-screen flex flex-col">
-        <div className="h-[65vh] w-full flex">
-          <div className="w-9/12 h-full flex items-center ">
-          <div className="flex flex-col gap-[2vw]">
-            <h1 className="font-secondary text-customGreen xl:text-[3.5vw] md:text-[4.5vw]  ml-[2.5vw] px-[3vw] tracking-wide leading-relaxed">
-              From Imagination <br /> to Adornment
-            </h1>
-            <h2 className="ml-[2.5vw] px-[3vw] text-[1.5vw] text-customBlack">
-              Welcome to KinMitra
-            </h2>
-            <div className="ml-[2.5vw] px-[3vw] flex gap-10">
-              <Link to="/form" className="rounded-full bg-customGreen px-[2vw] py-[1vw] text-white/80 md:text-[1.5vw] xl:text-[1vw]">
-                Design Now -&gt;
-              </Link>
-              <button className="flex justify-center items-center gap-3 md:text-[1.5vw] xl:text-[1vw] text-customGreen">
-                <div className="rounded-full border-2 border-customGreen p-[1vw]">
-                  <img src={img3} alt="" className="w-[1vw]" />
-                </div>
-                <div>Play Video</div>
-              </button>
+      <Navbar />
+      <div className="flex flex-col ">
+        <div className="bg-[#fff9f5] min-h-screen w-full flex flex-col  items-center gap-[10vh]">
+          <div className="flex gap-[3vh] flex-col items-center mt-[3vh]">
+            <div>
+              <img src={logo} alt="" className="w-[12rem]" />
+            </div>
+            <div className="text-customGreen xs:text-[2rem] text-center ">
+              <h1>
+                From Imagination <br /> To Adornment
+              </h1>
             </div>
           </div>
-          
+
+          <div className="bg-customBeige h-[30vh] w-full flex items-center justify-center ">
+            <img className="w-[45vh]" alt="Pexels arif" src={img21} />
           </div>
-          <div className="w-3/12 bg-[#f5e8d7] h-full flex items-center "></div>
-          <img
-            src={img21}
-            alt=""
-            className="absolute  md:right-[3vw] xl:right-[8vw] md:top-[25vh] xl:top-[18vh] xl:h-[50vh] md:h-[30vh]"
-          />
-        </div>
-        <div className="h-[20vh] w-full flex">
-          <div className="w-9/12 bg-[#f5e8d7] h-full flex items-center">
-            <div className="border-r border-[#b9944c] bg-customGreen md:h-[25vh] lg:h-[28vh] w-1/4 ">
-              <div className="w-[90%] flex justify-center">
-                <img src={img4} alt="" className="md:w-[11vh] lg:w-[13vh]" />
+
+          <div className="flex gap-[3vw] text-[1rem] justify-center items-center ">
+            <Link
+              to="/form"
+              className="bg-customGreen px-[2rem] py-[1rem] rounded-full text-customBeige"
+            >
+              <div>Design Now -&gt;</div>
+            </Link>
+
+            <button className="flex items-center gap-[1vw] text-customGreen justify-center">
+              <div className="rounded-full border-2 border-customGreen p-[0.8rem]">
+                <img src={img3} alt="" className="w-[1rem]" />
               </div>
-            </div>
-            <div className="border-r border-[#b9944c] w-1/4 h-full flex flex-col gap-[1.5vh] justify-center items-center text-customGreen">
-              <img src={img9} alt="" className=" md:w-[3vh] xl:w-[4vh]" />
-              <p className="text-center md:text-[1.5vh] xl:text-[2vh] xl:max-w-[60%]">
+              <p>Play Video</p>
+            </button>
+          </div>
+        </div>
+        <div className="min-h-screen flex justify-center items-center bg-[#fff9f5] sm:pt-[2rem] pb-[3rem]">
+          <div className="flex flex-col h-[80%] w-[60%] gap-[5vh] ">
+            <div className="bg-customBeige gap-[0.6rem] min-h-[10rem] rounded-lg border border-[#b9944c] flex flex-col justify-center items-center text-customGreen">
+              <img src={img9} alt="" className="size-[2rem]" />
+              <p className="max-w-[60%] text-center text-[1rem]">
                 Personalised Bespoke Designs
               </p>
             </div>
-            <div className="border-r border-[#b9944c] w-1/4 h-full flex flex-col gap-[1.5vh] justify-center items-center text-customGreen">
-              <img src={img8} alt="" className=" md:w-[3vh] xl:w-[4vh]" />
-              <p className="text-center md:text-[1.5vh] xl:text-[2vh] xl:max-w-[60%]">
+            <div className="bg-customBeige gap-[0.6rem] min-h-[10rem] rounded-lg border border-[#b9944c] flex flex-col justify-center items-center text-customGreen">
+              <img src={img8} alt="" className="size-[2rem]" />
+              <p className="max-w-[60%] text-center text-[1rem]">
                 Data-Driven Insights
               </p>
             </div>
-            <div className="border-r border-[#b9944c] w-1/4 h-full flex flex-col gap-[1.5vh] justify-center items-center text-customGreen">
-              <img src={img7} alt="" className=" md:w-[3vh] xl:w-[4vh]" />
-              <p className="text-center md:text-[1.5vh] xl:text-[2vh] xl:max-w-[60%]">
+            <div className="bg-customBeige gap-[0.6rem] min-h-[10rem] rounded-lg border border-[#b9944c] flex flex-col justify-center items-center text-customGreen">
+              <img src={img7} alt="" className="size-[2rem]" />
+              <p className="max-w-[60%] text-center text-[1rem]">
                 Seamless User Experience
               </p>
             </div>
           </div>
-          <div className="w-3/12 h-full"></div>
         </div>
       </div>
-
     </>
   );
 };
 
-export default Core;
+export default Core_2;
