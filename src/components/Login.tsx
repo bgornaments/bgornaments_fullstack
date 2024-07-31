09edd9f98e50e5708cb9500384be923ce7b2cb45
vanshika sharma <vanshikas2022@gmail.com>
2024-07-31 16:18:58 +0530

    Add login animation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,10 +2,21 @@ import React, { useEffect } from "react";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import { useNavigate } from "react-router-dom";
 import "@aws-amplify/ui-react/styles.css";
+import Lottie from "react-lottie";
+import login from "/src/assets/login.json";
 
 const Login: React.FC = () => {
   const { route } = useAuthenticator();
   const navigate = useNavigate();
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: login,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   useEffect(() => {
     if (route === "authenticated") {
@@ -16,9 +27,14 @@ const Login: React.FC = () => {
   }, [route, navigate]);
 
   return (
-    <div className="bg-[#FFF9F5] w-full min-h-screen flex flex-col  items-center justify-center">
-      <div className="bg-customGreen p-2 rounded-xl">
-        <Authenticator />
+    <div className="w-full min-h-screen flex">
+      <div className="w-1/2 min-h-screen bg-[#FFF9F5] flex items-center justify-center shadow-md ">
+        <Lottie options={defaultOptions} height={500} width={500} />
+      </div>
+      <div className="w-1/2 min-h-screen bg-white rounded-2xl flex items-center justify-center">
+        <div className="bg-customBeige p-1 rounded-xl">
+          <Authenticator />
+        </div>
       </div>
     </div>
   );
