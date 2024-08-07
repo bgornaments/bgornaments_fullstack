08826223a314327425015c1707c90465d6c8f9e1
vanshika sharma <vanshikas2022@gmail.com>
2024-08-07 12:42:14 +0530

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Login from "./components/Login.tsx";
 import { Authenticator } from "@aws-amplify/ui-react";
 import Catalogue from "./components/BrowseCatalogue/Catalogue.tsx"
 import DetailedImageView from "./components/BrowseCatalogue/DetailedImageView.tsx";
-import LikedImages from "./components/BrowseCatalogue/LikedImages.tsx";
+// import LikedImages from "./components/BrowseCatalogue/LikedImages.tsx";
 
 // const RequireAuth: React.FC<{ children: ReactNode }> = ({ children }) => {
 //   const { route } = useAuthenticator();
