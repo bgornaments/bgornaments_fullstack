3a26155ee0c753b80db7fad266f386f0b3b22eb9
bgornaments <bharatgold2023@gmail.com>
2025-01-04 11:50:48 +0530

    fixing issue

--- src/redux/authSlice.ts
+++ src/redux/authSlice.ts
@@ -1,4 +1,29 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// interface AuthState {
+//   user: string | null;
+// }
+
+// const initialState: AuthState = {
+//   user: null,
+// };
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState,
+//   reducers: {
+//     setUser(state, action: PayloadAction<string | null>) {
+//       state.user = action.payload;
+//     },
+//     clearUser(state) {
+//       state.user = null;
+//     },
+//   },
+// });
+
+// export const { setUser, clearUser } = authSlice.actions;
+// export default authSlice.reducer;
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
   user: string | null;
@@ -9,7 +34,7 @@ const initialState: AuthState = {
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
     setUser(state, action: PayloadAction<string | null>) {
