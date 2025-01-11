31d26cf09c901e64d72e59e0f1d336e02f96855f
bgornaments <bharatgold2023@gmail.com>
2025-01-11 09:42:15 +0530

    minor refinements in background color and sizes

--- src/components/ExpertMode/ExpertMode.tsx
+++ src/components/ExpertMode/ExpertMode.tsx
@@ -5,7 +5,7 @@ import imgvar from "/src/assets/image_variations_icon.jpg";
 
 const ExpertMode: React.FC = () => {
   return (
-    <div className="bg-[#FFF9F5] w-full min-h-screen flex justify-center items-center">
+    <div className="w-full min-h-screen flex justify-center items-center">
       <div className="xs:w-[90vw] xl:w-[75vw] flex flex-col justify-center items-center xs:gap-[2rem] md:gap-[10vh]">
         <div className="flex flex-col gap-[3vh] items-center">
           <h4 className="text-customGreen text-center xs:text-[1.2rem] md:text-[1.7rem] xl:text-[2rem] pt-[1rem]">
@@ -23,7 +23,7 @@ const ExpertMode: React.FC = () => {
               width="22vw"
               h4Text="Set Generator"
               h4Size="1.5vw"
-              pText="Effortlessly create custom data sets, optimized for your needs with precision and flexibility."
+              pText="Effortlessly create jewellery sets, optimized for your needs with flexibility."
               pSize="1vw"
               imgWidth="9vw"
               gap="2vw"
@@ -44,10 +44,10 @@ const ExpertMode: React.FC = () => {
           </div>
         </div>
         <div className="hidden md:block xl:hidden">
-          <div className="flex gap-[8vw]">
+          <div className="flex flex-col gap-[8vw]">
             <Card
               img={setgen}
-              height="50vh"
+              height="70vh"
               width="38vw"
               h4Text="Set Generator"
               h4Size="1.5rem"
@@ -59,7 +59,7 @@ const ExpertMode: React.FC = () => {
             />
             <Card
               img={imgvar}
-              height="50vh"
+              height="70vh"
               width="38vw"
               h4Text="Image Variation"
               h4Size="1.5rem"
