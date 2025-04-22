75df85ee16a7477f4e3ad4f67338a9f72d229824
Ayush Hurkat <ayushhurkat.22@gmail.com>
2025-04-22 15:54:45 +0530

    added profile link in navbar

--- src/landingNew/navbar.tsx
+++ src/landingNew/navbar.tsx
@@ -94,6 +94,15 @@ const Navbar: React.FC<NavbarProps> = ({ onContactClick, onFaqClick }) => {
             <span className="absolute bottom-[-4px] left-0 w-full h-[2px] bg-[#e0ae2a] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left" />
             <span className={`absolute bottom-[-4px] left-0 w-full h-[2px] bg-[#e0ae2a] ${activeLink === 'Contact Us' ? 'scale-x-100' : 'scale-x-0'}`} />
           </button>
+          <Link
+            to="/profile-page"
+            className={getLinkClass('Profile')}
+            onClick={() => setActiveLink('Profile')}
+          >
+            Profile
+            <span className="absolute bottom-[-4px] left-0 w-full h-[2px] bg-[#e0ae2a] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left" />
+            <span className={`absolute bottom-[-4px] left-0 w-full h-[2px] bg-[#e0ae2a] ${activeLink === 'Pricing' ? 'scale-x-100' : 'scale-x-0'}`} />
+          </Link>
         </div>
 
         {/* Login/Logout on right with right margin */}
